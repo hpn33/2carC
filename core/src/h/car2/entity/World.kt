565792acb63cb3551e7d5780ef530439833e1240
hamed pishdad <hamed.hpn332@gmail.com>
2019-01-11 20:36:33 +0330

    world is work

--- core/src/h/car2/entity/World.kt
+++ core/src/h/car2/entity/World.kt
@@ -2,33 +2,71 @@ package h.car2.entity
 
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import com.badlogic.gdx.utils.viewport.FitViewport
-import com.badlogic.gdx.utils.viewport.Viewport
-import h.car2.wHeight
-import h.car2.wWidth
-import ktx.graphics.rect
+import h.car2.util.*
 
-class World(assetManager: AssetManager) {
+class World(private val assetManager: AssetManager) {
 
 
-	private val shapeRenderer by lazy { ShapeRenderer() }
+	private val line by lazy { assetManager.atlas(RegionName.lineCenterStreet) }
+	private val centerLine by lazy { assetManager.atlas(RegionName.lineCenter1) }
+	private val street by lazy { assetManager.atlas(RegionName.street) }
+
+
+	var offSetLine = 0f
+	var offSetStreet = 0f
+
+
+	fun update(delta: Float) {
+
+
+		offSetLine -= delta
+
+		if (offSetLine < -line.regionHeight)
+			offSetLine = 0f
+
+		offSetStreet -= delta
+
+		if (offSetStreet < -street.regionHeight)
+			offSetStreet = 0f
+	}
+
 
 	fun draw(batch: SpriteBatch) {
 
 
-	}
+		var x = 0f
+		var y = offSetStreet
+		while (x < wWidth + street.regionWidth) {
+			while (y < wHeight + street.regionHeight) {
 
-	fun draw(viewport: Viewport) {
-		shapeRenderer.projectionMatrix = viewport.camera.combined
+				batch.draw(street, x, y)
+
+				y += street.regionHeight
+
+			}
+			y = offSetStreet
+			x += street.regionWidth
 
 
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+		}
 
+		y = offSetLine
+		while (y < wHeight + line.regionWidth) {
 
-		shapeRenderer.rect(0f, 0f, wWidth, wHeight)
+			val w = line.regionWidth / 2f
 
-		shapeRenderer.end()
+			batch.draw(line,
+					w linePosition 1/* - centerLine.regionWidth / 2*/, y,
+					w, line.regionHeight.toFloat())
+
+			batch.draw(centerLine, centerLine.regionWidth linePosition 2, y)
+
+			batch.draw(line, w linePosition 3/* + centerLine.regionWidth / 2*/, y,
+					w, line.regionHeight.toFloat())
+
+			y += line.regionHeight
+
+		}
 
 	}
 
